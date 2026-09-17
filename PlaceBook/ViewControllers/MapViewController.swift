6a79c47312ccaf8b817010539f10785c9ba88f57
Ahmet Cilingir <face4forev@icloud.com>
2026-09-17 23:09:34 +0200

    Initialize PlaceBook project structure

---
+++ PlaceBook/ViewControllers/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  PlaceBook
+//
+//  Created by Ahmet CILINGIR on 17.09.26.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Map"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+    }
+
+}
---
+++ PlaceBook/ViewControllers/MyPlacesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MyPlacesViewController.swift
+//  PlaceBook
+//
+//  Created by Ahmet CILINGIR on 17.09.26.
+//
+
+import UIKit
+
+class MyPlacesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "My Places"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+    }
+
+}
