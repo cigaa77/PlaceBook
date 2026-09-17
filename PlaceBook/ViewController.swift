cdb35502b447581d76121d4679f3f29936ae0460
Ahmet Cilingir <face4forev@icloud.com>
2026-09-17 22:51:19 +0200

    Initial Commit

---
+++ PlaceBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaceBook
+//
+//  Created by Ahmet CILINGIR on 17.09.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
